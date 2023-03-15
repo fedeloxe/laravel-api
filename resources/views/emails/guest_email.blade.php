06c55ab5e3adbe7fe0328324c1e83d68def96aa7
fedeloxe <loxeee@gmail.com>
2023-03-15 16:40:16 +0100

    non gliela faccio più

---
+++ resources/views/emails/guest_email.blade.php
@@ -0,0 +1,10 @@
+<h2>Nuovo contatto da boolpress</h2>
+
+<p>
+    nome: {{$lead->name}} <br>
+    congome: {{$lead->surname}} <br>
+    telefono:{{$lead->phone}} <br>
+    email:{{$lead->email}} <br>
+    contenuto: <br>
+    {{$lead->content}}
+</p>
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController as ProjectController;
+use App\Http\Controllers\Api\GuestLeadController as GuestLeadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use App\Http\Controllers\Api\ProjectController as ProjectController;
 
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{slug}', [ProjectController::class, 'show']);
+Route::post('/contacts', [GuestLeadController::class, 'store']);
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
